2519c1045d18bc6b32ba2a8c769b3464b56e6c13
lowercase-tousif <toisiftasrik@gmail.com>
2025-06-08 02:22:47 +0600

    Task 3: Complex Dynamic Routes Done

--- Blog-Admin-Panel-Routes/routes/web.php
+++ Blog-Admin-Panel-Routes/routes/web.php
@@ -22,5 +22,9 @@ Route::prefix("/admin")->group(function () {
         Route::get("/create", function () {
             return "<h1>Admin create posts here</h1>";
         })->name("create");
+
+        Route::get("/{id}/revisions/{revisionId}", function (string $id, string $revisionId) {
+            return "<h3>Post id: " . $id . "</h3>" . "<p>RevisionId: " . $revisionId . "</p>";
+        })->whereNumber('id')->whereAlphaNumeric('revisionId')->name("show");
     });
 });
