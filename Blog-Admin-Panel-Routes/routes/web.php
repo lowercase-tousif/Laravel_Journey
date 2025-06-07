06c18a67b21dc138f9ecbb777ecd2b98b9ee2cc2
lowercase-tousif <toisiftasrik@gmail.com>
2025-06-08 02:13:47 +0600

    Task 2: Nested Groups for Post Management

--- Blog-Admin-Panel-Routes/routes/web.php
+++ Blog-Admin-Panel-Routes/routes/web.php
@@ -13,4 +13,14 @@ Route::prefix("/admin")->group(function () {
     Route::get("/dashboard", function () {
         return "<h1>Welcome admin ! to the dashboard</h1>";
     });
+
+    Route::prefix("/posts")->group(function () {
+        Route::get("/", function () {
+            return "<h1>Showing all the posts</h1>";
+        })->name("index");
+
+        Route::get("/create", function () {
+            return "<h1>Admin create posts here</h1>";
+        })->name("create");
+    });
 });
