693827aff6f345dc4cc3bbf01989a0a46c56e41a
lowercase-tousif <toisiftasrik@gmail.com>
2025-06-08 02:08:01 +0600

    Task 1: The Admin Foundation Done

--- Blog-Admin-Panel-Routes/routes/web.php
+++ Blog-Admin-Panel-Routes/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix("/admin")->group(function () {
+    Route::redirect("/", "/admin/dashboard");
+
+    Route::get("/dashboard", function () {
+        return "<h1>Welcome admin ! to the dashboard</h1>";
+    });
+});
